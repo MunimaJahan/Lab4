726acea5dc2e50e7f8de29ffaceffd407d098fcb
MunimaJahan <59153345+MunimaJahan@users.noreply.github.com>
2021-03-01 15:56:48 -0700

    Update Main.java

--- src/comparator/Main.java
+++ src/comparator/Main.java
@@ -3,12 +3,12 @@ import java.util.Collections;
 public class Main {
   public static void main(String[] args) {
     ArrayList<Employee> Employees = new ArrayList<>();
-    Employees.add(new Employee("Carl", Employee.Grade.CPLUS, 1));
-    Employees.add(new Employee("Alice", Employee.Grade.A, 3));
-    Employees.add(new Employee("Bob", Employee.Grade.B, 7));
-    Employees.add(new Employee("Bill", Employee.Grade.BMINUS, 2));
-    Employees.add(new Employee("Bamford", Employee.Grade.BMINUS, 0));
-    Employees.add(new Employee("Carol", Employee.Grade.C, 6));
+    Employees.add(new Employee("Carl", 100, 1));
+    Employees.add(new Employee("Alice", 500, 3));
+    Employees.add(new Employee("Bob", 200, 7));
+    Employees.add(new Employee("Bill", 200, 2));
+    Employees.add(new Employee("Bamford", 700, 5));
+    Employees.add(new Employee("Carol", 100, 6));
     System.out.println("unsorted "+ Employees);
     Collections.sort(Employees);
     System.out.println("natural ordering"+ Employees);
